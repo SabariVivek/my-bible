9b4489987568f67011bc91ac7bae2a69f2150bf8
Sabari D V <sabari.v@redcross.com>
2025-11-20 12:43:46 +0530

    Remove character definitions for Psalms, Ruth, Song of Solomon, Zechariah, and Zephaniah from the Old Testament resources.

--- resources/characters/old-testament/obadiah.js
+++
@@ -1,8 +0,0 @@
-﻿// obadiah Characters
-window.obadiahCharacters = {
-	1: [
-		{ name: "Obadiah", description: "Saw a vision concerning Edom." },
-		{ name: "An Edomite", description: "Was warned of being brought down from the heights." },
-		{ name: "A Survivor in Zion", description: "Would possess the house of Esau." }
-	]
-};
