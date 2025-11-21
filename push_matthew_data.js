76c19a63d12a8a98b03f5e98bf04571c67c0cec0
Sabari D V <sabari.v@redcross.com>
2025-11-21 09:59:47 +0530

    Remove outdated RLS policies for chapter_timeline table and replace with new public access policies.

--- push_matthew_data.js
+++
@@ -1,8 +0,0 @@
-// Run this in browser console when on Matthew 1 to push timeline data
-// Make sure you're logged in as admin
-
-if (typeof pushMatthew1TimelineData === 'function') {
-    pushMatthew1TimelineData();
-} else {
-    console.log('pushMatthew1TimelineData function not found. Make sure the script is loaded.');
-}
